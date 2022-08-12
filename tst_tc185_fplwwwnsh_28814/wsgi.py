bc73a712f8171ce64deaa9bccb9be5609dc441d3
Crowdbotics <engineering@crowdbotics.com>
2022-08-12 00:33:03 +0000

    Update models

--- tst_tc185_fplwwwnsh_28814/wsgi.py
+++ tst_tc185_fplwwwnsh_28814/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tst_tc185_fplwwwnsh_28814.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tst_tc185_fplwwwnsh_28814.settings")
 
 application = get_wsgi_application()
